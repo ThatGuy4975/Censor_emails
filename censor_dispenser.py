2172b87bb3801e0ff3a9876ec55c90dc452d8059
Rylee <ryleeforeman@gmail.com>
2020-10-28 12:37:50 -0500

    Second commit, seperated censor functions to the outside of the main censor function.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -5,20 +5,32 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
-def censor_single_phrase(email_input) :
-	#defining word to be censored
-	censored_phrase = "learning algorithms"
-	if censored_phrase in email_input :
-		new_input = email_input.replace(censored_phrase, "[Redacted]")
-	return new_input
+# def censor_single_phrase(email_input) :
+# 	#defining word to be censored
+# 	censored_phrase = "learning algorithms"
+# 	if censored_phrase in email_input :
+# 		new_input = email_input.replace(censored_phrase, "[Redacted]")
+# 	return new_input
 
-def censor_multiple_phrases(email_input) :
-	#splitting the email input into seperate words, and checking to see if words were split properly
-	split_email_input = email_input.split(' ')
-	# print(split_email_input)
+def censor_open_words(old_input) :
+	split_email_input = old_input.split(' ')
 	new_input = []
-	#checking each word in split email to see if it should be censored
 	for word in split_email_input :
+		#for words without paragraphs
+			if word.lower() in proprietary_terms :
+				for letter in word :
+					new_input.append('*')
+				new_input.append(' ')
+			else :
+				new_input.append(word + ' ')
+
+	joined_email_input = ''.join(new_input)
+	return joined_email_input
+
+def censor_paragraph_words(old_input) :
+	split_input = old_input.split(' ')
+	new_input = []
+	for word in split_input :
 		#for words without paragraphs
 		if "\n" not in word:
 			if word.lower() in proprietary_terms :
@@ -42,11 +54,24 @@ def censor_multiple_phrases(email_input) :
 					else:
 						new_input.append(word + '\n')
 
-	joined_email_input = ''.join(new_input)
+	joined_email_input = ''.join(new_input)				
 	return joined_email_input
-	# split_email_input = joined_email_input.split("\n\n")
-	# print(split_email_input)
+
+
+
+def censor_multiple_phrases(email_input) :
+	before_censorship = email_input
+
+	initial_censor = censor_open_words(before_censorship)
+	# print(initial_censor)
+
+	second_censor = censor_paragraph_words(initial_censor)
+	# print(second_censor)
+
+
+
+
 
 
 # print(censor_single_phrase(email_one))
-print(censor_multiple_phrases(email_two))
+censor_multiple_phrases(email_two)
